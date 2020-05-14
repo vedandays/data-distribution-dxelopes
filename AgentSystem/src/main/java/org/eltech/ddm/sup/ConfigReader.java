69a93770eea34d5b6c302e321a7a899a751ea7b5
Andrey Vedenin <vedandays@gmail.com>
2020-05-14 22:02:45 +0300

    Правки после мерджа.

--- AgentSystem/src/main/java/org/eltech/ddm/sup/ConfigReader.java
+++ AgentSystem/src/main/java/org/eltech/ddm/sup/ConfigReader.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 /*reading file with agents information */
 
 public class ConfigReader {
-    public static  ArrayList<AgentInfo> readFile(String filePath){
+   /* public static  ArrayList<AgentInfo> readFile(String filePath){
         String line;
         String[] lines;
         String splitBySymbol = ",";
@@ -32,5 +32,5 @@ public class ConfigReader {
         }
 
         return agentInfoArrayList;
-    }
+    }*/
 }
